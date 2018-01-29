d27892a8fb89f0c010dbe568afa29f0fb1bbdf2b
phobal <phobal@126.com>
2018-01-29 11:40:28 +0800

    add mongoose options

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 const dbUrl = require('../config/config').db;
 
-exports.connection = function() {
+exports.connection = function () {
+  const options = {
+    server: {
+      auto_reconnect: true,
+      poolSize: 10
+    }
+  }
   mongoose.connect(dbUrl);
   var db = mongoose.connection;
-  db.on('error', function() { console.error('connection error') });
-  db.once('open', function() {
+  db.on('error', function () { console.error('connection error') });
+  db.once('open', function () {
     console.log('connection success!');
   });
 }
 
-exports.disconnection = function(callback) {
+exports.disconnection = function (callback) {
   mongoose.disconnect(callback);
 }
